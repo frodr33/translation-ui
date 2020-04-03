1dac877e40e9c713f6b3a26a23d4dfff6b8f6642
frodr33 <fsr32@cornell.edu>
2020-04-02 22:17:38 -0400

    test for cors

--- src/App.js
+++ src/App.js
@@ -22,14 +22,143 @@ class App extends Component {
   constructor(props) {
 
     // Creating Unique User ID
-    let name = prompt("Please enter your name", "Harry Potter");
+    let name = prompt("Please enter your name");
+    let language = prompt("Please enter your preferred language (Will change this to dropdown)", "English");
+    language = language.toLowerCase();
+
     let user_id =  name + '_' + Math.random().toString(36).substr(2, 9) + ":";
 
-    console.log(user_id)
+    let key_to_language_map = {
+      'af': 'afrikaans',
+      'sq': 'albanian',
+      'am': 'amharic',
+      'ar': 'arabic',
+      'hy': 'armenian',
+      'az': 'azerbaijani',
+      'eu': 'basque',
+      'be': 'belarusian',
+      'bn': 'bengali',
+      'bs': 'bosnian',
+      'bg': 'bulgarian',
+      'ca': 'catalan',
+      'ceb': 'cebuano',
+      'ny': 'chichewa',
+      'zh-cn': 'chinese (simplified)',
+      'zh-tw': 'chinese (traditional)',
+      'co': 'corsican',
+      'hr': 'croatian',
+      'cs': 'czech',
+      'da': 'danish',
+      'nl': 'dutch',
+      'en': 'english',
+      'eo': 'esperanto',
+      'et': 'estonian',
+      'tl': 'filipino',
+      'fi': 'finnish',
+      'fr': 'french',
+      'fy': 'frisian',
+      'gl': 'galician',
+      'ka': 'georgian',
+      'de': 'german',
+      'el': 'greek',
+      'gu': 'gujarati',
+      'ht': 'haitian creole',
+      'ha': 'hausa',
+      'haw': 'hawaiian',
+      'iw': 'hebrew',
+      'hi': 'hindi',
+      'hmn': 'hmong',
+      'hu': 'hungarian',
+      'is': 'icelandic',
+      'ig': 'igbo',
+      'id': 'indonesian',
+      'ga': 'irish',
+      'it': 'italian',
+      'ja': 'japanese',
+      'jw': 'javanese',
+      'kn': 'kannada',
+      'kk': 'kazakh',
+      'km': 'khmer',
+      'ko': 'korean',
+      'ku': 'kurdish (kurmanji)',
+      'ky': 'kyrgyz',
+      'lo': 'lao',
+      'la': 'latin',
+      'lv': 'latvian',
+      'lt': 'lithuanian',
+      'lb': 'luxembourgish',
+      'mk': 'macedonian',
+      'mg': 'malagasy',
+      'ms': 'malay',
+      'ml': 'malayalam',
+      'mt': 'maltese',
+      'mi': 'maori',
+      'mr': 'marathi',
+      'mn': 'mongolian',
+      'my': 'myanmar (burmese)',
+      'ne': 'nepali',
+      'no': 'norwegian',
+      'ps': 'pashto',
+      'fa': 'persian',
+      'pl': 'polish',
+      'pt': 'portuguese',
+      'pa': 'punjabi',
+      'ro': 'romanian',
+      'ru': 'russian',
+      'sm': 'samoan',
+      'gd': 'scots gaelic',
+      'sr': 'serbian',
+      'st': 'sesotho',
+      'sn': 'shona',
+      'sd': 'sindhi',
+      'si': 'sinhala',
+      'sk': 'slovak',
+      'sl': 'slovenian',
+      'so': 'somali',
+      'es': 'spanish',
+      'su': 'sundanese',
+      'sw': 'swahili',
+      'sv': 'swedish',
+      'tg': 'tajik',
+      'ta': 'tamil',
+      'te': 'telugu',
+      'th': 'thai',
+      'tr': 'turkish',
+      'uk': 'ukrainian',
+      'ur': 'urdu',
+      'uz': 'uzbek',
+      'vi': 'vietnamese',
+      'cy': 'welsh',
+      'xh': 'xhosa',
+      'yi': 'yiddish',
+      'yo': 'yoruba',
+      'zu': 'zulu',
+      'fil': 'Filipino',
+      'he': 'Hebrew'
+    }
+
+    let language_to_key_map = Object.fromEntries(Object.entries(key_to_language_map).map(([k,v], i) => [v,k]))
+
+    while (!language_to_key_map[language]) {
+      language = prompt("Sorry, please pick a different language", "English");
+      language = language.toLowerCase()
+    }
+
+    let lang = language_to_key_map[language] + ":"
+
+    // SET UP LOGIC
+    let language_selection_metadata = "IGNORE:" + language_to_key_map[language]
+    console.log("Sending language selection: " + language_to_key_map[language])
+    submitWebSocket.send(language_selection_metadata)
+
+    // GET REQUEST CONNECT
+    fetch("http://translation-backend.herokuapp.com/connect")
+    .then(res => console.log(res))
 
     super(props);
     this.state = {
       id: user_id,
+      lang: '',
       value: '',
       messages: []
     };
@@ -38,8 +167,19 @@ class App extends Component {
       let messageRecevied = event.data;
       let message_id;
 
-      console.log("received message: " + messageRecevied)
+      if (messageRecevied.indexOf("IGNORE") != -1) {
+        // Setup message
+        let langIndex = messageRecevied.indexOf(":") + 1;
+        let lang = messageRecevied.substring(langIndex)
 
+        console.log("setting preferred language")
+        this.setState({
+          lang: lang + ":"
+        })
+        return
+      }
+
+      console.log("received message: " + messageRecevied)
       console.log(messageRecevied.indexOf(this.state.id))
 
       if (messageRecevied.indexOf(this.state.id) != -1) {
@@ -49,7 +189,9 @@ class App extends Component {
         message_id = RECIPIENT_ID;
       }
 
-      let messageIndex = messageRecevied.indexOf(":") + 1;
+      let langIndex = messageRecevied.indexOf(":") + 1;
+      let messageIndex = messageRecevied.indexOf(":", langIndex) + 1;
+
       messageRecevied = messageRecevied.substring(messageIndex);
 
       // Create new message
@@ -80,7 +222,7 @@ class App extends Component {
     console.log("Sending '" + this.state.value + "' to backend")
 
     // Prepend userid
-    let newMsg = this.state.id + this.state.value;
+    let newMsg = this.state.id + this.state.lang + this.state.value;
     console.log(newMsg);
 
     submitWebSocket.send(newMsg)
