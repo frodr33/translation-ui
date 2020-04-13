f46da21c413cac8408a380e23e4e1ccb5b76cdf4
Jiayi Xu <jx347@cornell.edu>
2020-04-12 18:18:10 -0700

    audio

--- src/App.js
+++ src/App.js
@@ -1,142 +1,180 @@
-import React, { Component } from 'react';
-import ReactLoading from 'react-loading';
-import { ChatFeed, Message } from 'react-chat-ui'
-import './App.css';
-import {Sockets, submitWebSocket, USER_ID} from './components/sockets'
-import Languages from './components/languages'
+import React, { Component } from "react";
+import ReactLoading from "react-loading";
+import { ChatFeed, Message } from "react-chat-ui";
+import "./App.css";
+import { Sockets, submitWebSocket, USER_ID } from "./components/sockets";
+import Languages from "./components/languages";
+import { ReactMic } from "react-mic";
+import Button from "@material-ui/core/Button";
+import Stop from "@material-ui/icons/Stop";
+import KeyboardVoiceIcon from "@material-ui/icons/KeyboardVoice";
 
-
-class App extends Component { 
+class App extends Component {
   constructor(props) {
     super(props);
-    let language = prompt("Please enter your preferred language (Will change this to dropdown)", "English");
+    let language = prompt(
+      "Please enter your preferred language (Will change this to dropdown)",
+      "English"
+    );
     language = language.toLowerCase();
-    
+
     while (!Languages[language]) {
       language = prompt("Sorry, please pick a different language", "English");
-      language = language.toLowerCase()
+      language = language.toLowerCase();
     }
 
     this.state = {
-      id: '',
+      id: "",
       ownLanguage: Languages[language],
       lang: Languages[language],
-      message: '',
+      message: "",
       messages: [],
       typing: false,
-      name: ''
+      name: "",
+      isRecording: false, 
+      blobURL: null
     };
   }
 
   /**
-   * Setting whether or not the user is able to 
+   * Setting whether or not the user is able to
    * type a message to the other user
    */
-  setTyping = (typingBool) => {
+  setTyping = typingBool => {
     this.setState(() => {
       return {
         typing: typingBool
-      }
-    })    
-  }
+      };
+    });
+  };
 
   /**
    * Setting the language of the other client
    */
-  setReceipientLanguage = (lang) => {
+  setReceipientLanguage = lang => {
     this.setState(() => {
       return {
         lang: lang
-      }
-    })
-  }
+      };
+    });
+  };
 
   /**
    * Handler for when a message is received by this client
    */
-  receiveMessage = (message) => {
+  receiveMessage = message => {
     let msgs = this.state.messages;
     msgs.push(message);
-    
+
     this.setState(() => {
       return {
         messages: msgs
-      }
-    })
-  }
-
+      };
+    });
+  };
 
   /**
    * On submission of the message form, creates a chat message object
-   * and appends it to the messages list kept in the state as well as 
+   * and appends it to the messages list kept in the state as well as
    * sends the message to the other client through submitWebSocket
    */
-  messageSubmit = (event) => {
-    event.preventDefault()
+  messageSubmit = event => {
+    event.preventDefault();
 
     // Create new message
     let msg = new Message({
-      id: USER_ID, 
+      id: USER_ID,
       message: this.state.message
-    })
-  
+    });
+
     let msgs = this.state.messages;
     msgs.push(msg);
 
     this.setState(() => {
       return {
         messages: msgs
-      }
-    })
+      };
+    });
 
     // Prepend userid
     let messageSent = this.state.id + this.state.lang + this.state.message;
     console.log("Sent wrapped message: " + messageSent);
 
-    submitWebSocket.send(messageSent)
+    submitWebSocket.send(messageSent);
     this.setState(() => {
       return {
         messages: msgs
-      }
-    })
-  }
+      };
+    });
+  };
 
   /**
    * On submission of the enter name form, creates a randomly generated
    * user ID for this client
    */
-  nameChangeSubmit = (event) => {
-    event.preventDefault()
-    let user_id =  this.state.name + '_' + Math.random().toString(36).substr(2, 9) + ":";
-    this.setState({id: user_id});
-  }
+  nameChangeSubmit = event => {
+    event.preventDefault();
+    let user_id =
+      this.state.name +
+      "_" +
+      Math.random()
+        .toString(36)
+        .substr(2, 9) +
+      ":";
+    this.setState({ id: user_id });
+  };
 
-  nameChangeHandler = (event) => {
-    this.setState({name: event.target.value});
-  }
+  nameChangeHandler = event => {
+    this.setState({ name: event.target.value });
+  };
 
-  messageChangeHandler = (event) => {
-    this.setState({message: event.target.value});
-  }
+  messageChangeHandler = event => {
+    this.setState({ message: event.target.value });
+  };
+
+  //mic start
+  onStart = () => {
+    console.log("You can tap into the onStart callback");
+  };
+
+  onStop = blobObject => {
+    this.setState({ blobURL: blobObject.blobURL });
+    //not sure which should be sent, can try different format
+    //console.log(blobObject.blobURL);
+    //console.log(blobObject.blob);
+  };
+
+  startRecording = () => {
+    this.setState({
+      isRecording: true
+    });
+  };
+
+  stopRecording = () => {
+    this.setState({
+      isRecording: false
+    });
+  };
 
   render() {
     return (
       <div>
         {this.state.id ? (
-          <Sockets 
-          receiveMessage={this.receiveMessage} userId={this.state.id} 
-          langKey={this.state.lang} 
-          setReceipientLanguage={this.setReceipientLanguage}
-          setTypingOn={this.setTypingOn}
-          ownLangKey={this.state.ownLanguage}
-          setTyping={this.setTyping}
-          >
-          </Sockets>
+          <Sockets
+            receiveMessage={this.receiveMessage}
+            userId={this.state.id}
+            langKey={this.state.lang}
+            setReceipientLanguage={this.setReceipientLanguage}
+            setTypingOn={this.setTypingOn}
+            ownLangKey={this.state.ownLanguage}
+            setTyping={this.setTyping}
+            recordMessage={this.blobURL}
+          ></Sockets>
         ) : (
           <div className="App" onSubmit={this.nameChangeSubmit}>
             <form>
               <p>Enter Name:</p>
-              <input type="text" onChange={this.nameChangeHandler}/>
+              <input type="text" onChange={this.nameChangeHandler} />
             </form>
           </div>
         )}
@@ -145,24 +183,69 @@ class App extends Component {
           <div className="App" onSubmit={this.messageSubmit}>
             <form>
               <p>Enter message:</p>
-              <input type="text" onChange={this.messageChangeHandler}/>
+              <input type="text" onChange={this.messageChangeHandler} />
             </form>
+            <ReactMic
+              record={this.state.isRecording}
+              width={0}
+              height={0}
+              onStop={this.onStop}
+              onStart={this.onStart}
+            />
+            {this.state.isRecording ? (
+              <Button
+                variant="contained"
+                color="secondary"
+                disabled
+                startIcon={<KeyboardVoiceIcon />}
+                onClick={this.startRecording}
+              >
+                Talk
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="secondary"
+                startIcon={<KeyboardVoiceIcon />}
+                onClick={this.startRecording}
+              >
+                Talk
+              </Button>
+            )}
+
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<Stop />}
+              onClick={this.stopRecording}
+            >
+              Stop
+            </Button>
+            <audio
+              controls="controls"
+              src={this.state.blobURL}
+              controlsList="nodownload"
+            />
           </div>
-        ): (
+        ) : (
           <div>
-            <ReactLoading type={"spin"} color={"blue"} height={"10%"} width={"10%"} />
+            <ReactLoading
+              type={"spin"}
+              color={"blue"}
+              height={"10%"}
+              width={"10%"}
+            />
             <p>Waiting for chat room to fill up</p>
           </div>
         )}
 
         <ChatFeed
-        messages={this.state.messages}
-        isTyping={this.state.is_typing}
-        hasInputField={false}
-        showSenderName
-        bubblesCentered={false}
-        bubbleStyles={
-          {
+          messages={this.state.messages}
+          isTyping={this.state.is_typing}
+          hasInputField={false}
+          showSenderName
+          bubblesCentered={false}
+          bubbleStyles={{
             text: {
               fontSize: 30
             },
@@ -170,10 +253,9 @@ class App extends Component {
               borderRadius: 70,
               padding: 40
             }
-          }
-        }
+          }}
         />
-    </div>
+      </div>
     );
   }
 }
--- src/components/sockets.jsx
+++ src/components/sockets.jsx
@@ -1,129 +1,131 @@
-import React, { Component } from 'react';
-import ReconnectingWebSocket from 'reconnecting-websocket';
-import { Message } from 'react-chat-ui'
+import React, { Component } from "react";
+import ReconnectingWebSocket from "reconnecting-websocket";
+import { Message } from "react-chat-ui";
 
-let myKey = ""
+let myKey = "";
 
 const USER_ID = 0;
-const RECIPIENT_ID = 1
+const RECIPIENT_ID = 1;
 
+const PROD_URL = "translation-backend.herokuapp.com"; // Will eventually put this env variables
+const DEV_URL = "localhost:8000";
+const baseURL = DEV_URL;
 
-const PROD_URL = "translation-backend.herokuapp.com" // Will eventually put this env variables
-const DEV_URL = "localhost:5000"
+const submitWebSocket = new ReconnectingWebSocket(
+  "ws://" + baseURL + "/submit"
+);
+const receiveWebSocket = new ReconnectingWebSocket(
+  "ws://" + baseURL + "/receive"
+);
+const notificationWebSocket = new ReconnectingWebSocket(
+  "ws://" + baseURL + "/interruptions"
+);
 
-const baseURL = DEV_URL
-
-const submitWebSocket = new ReconnectingWebSocket('ws://' + baseURL + '/submit');
-const receiveWebSocket = new ReconnectingWebSocket('ws://' + baseURL + '/receive');
-const notificationWebSocket = new ReconnectingWebSocket('ws://' + baseURL + '/interruptions');
-
-
-submitWebSocket.addEventListener('open', () => {
-    console.log("Establishing initial /submit socket connection")
-});
-  
-receiveWebSocket.addEventListener('open', () => {
-    console.log("Establishing initial /receive socket connection")
-});
-
-notificationWebSocket.addEventListener('open', () => {
-    console.log("Establishing initial /interruptions socket connection")
+submitWebSocket.addEventListener("open", () => {
+  console.log("Establishing initial /submit socket connection");
 });
 
+receiveWebSocket.addEventListener("open", () => {
+  console.log("Establishing initial /receive socket connection");
+});
+
+notificationWebSocket.addEventListener("open", () => {
+  console.log("Establishing initial /interruptions socket connection");
+});
 
 class Sockets extends Component {
-    constructor(props) {
-        super(props)
-        myKey = this.props.ownLangKey
+  constructor(props) {
+    super(props);
+    myKey = this.props.ownLangKey;
 
-        receiveWebSocket.onmessage = (event) => {
-            let messageRecevied = event.data;
-            let message_id;
-        
-            if (messageRecevied.indexOf(this.props.userId) === -1) {
-              message_id = RECIPIENT_ID;
-              let langIndex = messageRecevied.indexOf(":") + 1;
-              let messageIndex = messageRecevied.indexOf(":", langIndex) + 1;
-        
-              messageRecevied = messageRecevied.substring(messageIndex);
-        
-              let msg = new Message({
-                id: message_id, 
-                message: messageRecevied
-              })
-        
-              console.log("Message received: " + messageRecevied)
-              this.props.receiveMessage(msg)
-            }
+    receiveWebSocket.onmessage = event => {
+      let messageRecevied = event.data;
+      let message_id;
+
+      if (messageRecevied.indexOf(this.props.userId) === -1) {
+        message_id = RECIPIENT_ID;
+        let langIndex = messageRecevied.indexOf(":") + 1;
+        let messageIndex = messageRecevied.indexOf(":", langIndex) + 1;
+
+        messageRecevied = messageRecevied.substring(messageIndex);
+
+        let msg = new Message({
+          id: message_id,
+          message: messageRecevied
+        });
+
+        console.log("Message received: " + messageRecevied);
+        this.props.receiveMessage(msg);
+      }
+    };
+
+    notificationWebSocket.onmessage = event => {
+      let messageRecevied = event.data;
+      this.props.setTyping(messageRecevied === "2");
+    };
+  }
+
+  async componentDidMount() {
+    console.log("Registering window close event listener");
+    console.log("ownkey: " + this.props.ownLangKey);
+
+    window.onbeforeunload = async function() {
+      console.log(myKey);
+      let disconnectUrl = "http://" + baseURL + "/disconnect?lang=" + myKey;
+      await fetch(disconnectUrl, {
+        method: "GET",
+        headers: {
+          "access-control-allow-origin": "*"
         }
+      }).then(res => console.log(res));
 
-        notificationWebSocket.onmessage = (event) => {
-            let messageRecevied = event.data;
-            this.props.setTyping(messageRecevied === "2")
-        }
-    }
+      return "Do you really want to close?";
+    };
 
-    async componentDidMount() {
-        console.log("Registering window close event listener")
-        console.log("ownkey: " + this.props.ownLangKey)
+    console.log("lang key: " + this.props.langKey);
+    let fetchUrl =
+      "http://" +
+      baseURL +
+      "/connect?lang=" +
+      this.props.langKey +
+      "&id=" +
+      this.props.userId;
+    console.log("Connecting to backend at: " + fetchUrl);
 
-
-        window.onbeforeunload = async function () {
-            console.log(myKey)
-            let disconnectUrl = 'http://' + baseURL + "/disconnect?lang=" + myKey
-            await fetch(disconnectUrl,{
-              method: 'GET',
-              headers: {
-                "access-control-allow-origin" : "*",
-              }
-            })
-            .then(res => console.log(res))
-      
-            return "Do you really want to close?"
-        }
-
-        console.log("lang key: " + this.props.langKey)
-        let fetchUrl = 'http://' + baseURL + "/connect?lang=" + this.props.langKey + "&id=" + this.props.userId
-        console.log("Connecting to backend at: " + fetchUrl)
-
-        let otherLanguage = "" // default
-        let getRequest = fetch(fetchUrl ,{
-          method: 'GET',
-          headers: {
-            "access-control-allow-origin" : "*",
+    let otherLanguage = ""; // default
+    let getRequest = fetch(fetchUrl, {
+      method: "GET",
+      headers: {
+        "access-control-allow-origin": "*"
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log("IN SOCKETS JSX");
+        console.log(res);
+        res.forEach((v, i) => {
+          if (v !== this.props.langKey) {
+            console.log("Other client's language is: " + v);
+            otherLanguage = v + ":";
           }
-        })
-        .then(res => res.json())
-        .then(res => {
-          console.log("IN SOCKETS JSX")
-          console.log(res);
-          res.forEach((v, i) => {
-              if (v !== this.props.langKey) {
-                  console.log("Other client's language is: " + v);
-                  otherLanguage = v + ":";
-              }
-          })    
+        });
 
-          if (otherLanguage === "") {
-              // Both clients choose the same language
-              otherLanguage = this.props.langKey + ":"
-          }
+        if (otherLanguage === "") {
+          // Both clients choose the same language
+          otherLanguage = this.props.langKey + ":";
+        }
 
-        this.props.setReceipientLanguage(otherLanguage)          
-        })
+        this.props.setReceipientLanguage(otherLanguage);
+      });
 
-        await getRequest;
-        this.props.setTyping(true)
-        console.log("Finished connecting successfully")
-    }
+    await getRequest;
+    this.props.setTyping(true);
+    console.log("Finished connecting successfully");
+  }
 
-    render() {
-        return (
-            <div>
-            </div>
-        )
-    }
-
+  render() {
+    return <div></div>;
+  }
 }
 
-export {Sockets, submitWebSocket, USER_ID}
+export { Sockets, submitWebSocket, USER_ID };
